5927cd1772c283e206b0176b45656e760766174c
Tech-chandu1991 <chandududekula1991@gmail.com>
2026-01-07 20:54:24 +0530

    python basics update

---
+++ Areas_circumference_calculations.py
@@ -0,0 +1,26 @@
+#Area of circle calculation direct calculation
+#radius = 8
+#phi = 3.14
+#area_of_circle = phi * radius ** 2
+#print("Area of Circle : ", area_of_circle)
+
+#Area of Circle Based on User selection inputs
+#radis = float(input("Radius: "))
+#phi = float(input("PHI: "))
+#Area = phi * radis ** 2
+#print("Area of Circle: ", Area)
+
+# circumference of circle calculation direct calculation
+#radius = 8
+#phi = 3.14
+#Circumference = 2 * radius * phi
+#print("Circumference of Circle: ", Circumference)
+
+# circumference of circle based on user inputs
+#radis = float(input("Radius:"))
+#Phi = float(input("PHI: "))
+#Circumference = 2 * Phi * radis
+#print("Circumference of Circle: ", Circumference)
+
+#
+
