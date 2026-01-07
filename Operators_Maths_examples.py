c354ebee28673979c9afefda0c9cea82b72c9cae
Tech-chandu1991 <chandududekula1991@gmail.com>
2026-01-07 22:18:30 +0530

    Math Operators

---
+++ Operators_Maths_examples.py
@@ -0,0 +1,23 @@
+# Area of Triangle based on user inputs prompts
+#base = int(input("Enter Base: "))
+#height = int(input("Enter Height: "))
+#Area = 0.5 * base * height
+#print("Area of Triangle: ", Area)
+
+# Perimeter of a Triangle based on user input prompts
+#side_a = int(input("Enter side A: "))
+#side_b = int(input("Enter side B: "))
+#side_c = int(input("Enter side C: "))
+#Perimeter = side_a + side_b + side_c
+#print("Perimeter of Trinagle : ", Perimeter)
+
+# Area of rectangle and perimeter of rectangle using prompt
+#length = float(input("Enter the Length of Rectangle: "))
+#width = float(input("Enter the Width of Rectangle: "))
+#Area = length * width
+#print("Area of Rectangle: ", Area)
+#perimeter = 2* (length + width)
+#print("Perimeter of Rectangle: ", perimeter)
+
+
+
