e55f2deed6939341b9865fa1edad86e042993b93
Tech-chandu1991 <chandududekula1991@gmail.com>
2026-01-28 09:51:08 +0530

    Tuples

---
+++ Tuples_in_python.py
@@ -0,0 +1,75 @@
+# Tuples in Python
+
+# Access tuple items using positive indexing
+#fruits = ('banana', 'orange', 'mango', 'lemon')
+#first_fruit = fruits[0]
+#print(first_fruit)
+#second_fruit =fruits[1]
+#print(second_fruit)
+#last_fruit = fruits[-1]
+#print(last_fruit)
+#last_index = len(fruits) -1
+#print(last_index)
+#Last_fruit = fruits[last_index]
+#print(Last_fruit)
+
+# Access tuple items using negative indexing
+#fruits = ('banana', 'orange', 'mango', 'lemon')
+#first_item = fruits[-4]
+#second_item = fruits[-3]
+#last_item = fruits[-1]
+#print(first_item)
+#print(second_item)
+#print(last_item)
+
+# slice(cut) the tuple items using positive indexing
+#fruits = ('banana', 'orange', 'mango', 'lemon')
+#all_fruit = fruits[0:]
+#print(all_fruit)
+#last_two_fruits = fruits[2:4]
+#print(last_two_fruits)
+#middle_fruits = fruits[1:3]
+#print(middle_fruits)
+#exclude_first_fruit = fruits[1:]
+#print(exclude_first_fruit)
+
+# slice(cut) the tuple items using negative indexing
+#fruits = ('banana', 'orange', 'mango', 'lemon')
+#all_fruits = fruits[-4:]
+#print(all_fruits)
+#last_two_fruits = fruits[-2:]
+#print(last_two_fruits)
+#middle_fruits = fruits[-3:-1]
+#print(middle_fruits)
+#exclude_first_item = fruits[-3:]
+#print(exclude_first_item)
+#exlude_last_item = fruits[-4:-1]
+#print(exlude_last_item)
+
+# changing tuple into list
+#fruits = ('banana', 'orange', 'mango', 'lemon')
+#fruits = list(fruits)
+#print(fruits)
+#fruits[0] = 'apple'
+#print(fruits)
+
+# Checking an item in tuple using 'in' keyword
+#fruits = ('banana', 'orange', 'mango', 'lemon')
+#print('mango' in fruits)
+#print('orange' in fruits)
+#print('grape' in fruits)
+
+# Joining the tuples using + operator
+#fruits = ('banana', 'orange', 'mango', 'lemon')
+#print(fruits)
+#vegetables = ('carrot', 'beetroot', 'ladiesfinger')
+#print(vegetables)
+#fruits_vegetables = fruits + vegetables
+#print(fruits_vegetables)
+
+# delete the tuple using del keyword
+fruits = ('banana', 'orange', 'mango', 'lemon')
+del fruits
+print(fruits)
+
+
